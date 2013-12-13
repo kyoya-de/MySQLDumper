d18c5971607134405ed0c627ee0aaeff02dbe7a5
DSB <admin@mysqldumper.de>
2013-12-13 22:23:11 +0100

    Fixed vagranbt box download url. Deleted composer.lock file.

--- web/config.php
+++ web/config.php
@@ -3,13 +3,10 @@
 if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
-    
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
-    'fe80::1',
     '::1',
-    '192.168.56.1',
-    'fe80::800:27ff:fe00:0'
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
